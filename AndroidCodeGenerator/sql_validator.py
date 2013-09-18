7b603952c0d799d1159570c2bdb8e8902edcff12
Jonas Kalderstam <jonas@kalderstam.se>
2013-09-18 11:46:33 +0200

    Created a class that tests your sql schema by creating an sqlite-database in python and creating everything in there. Plan to add basic insert/update/delete tests as well.
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

---
+++ AndroidCodeGenerator/sql_validator.py
@@ -0,0 +1,58 @@
+from __future__ import print_function, division
+import sqlite3 as sql
+import os
+
+def clear_db(func):
+    '''Removes the db-file before and after
+    function call'''
+    def _wrap(*args, **kwargs):
+        try:
+            os.remove('test.db')
+        except OSError:
+            pass
+
+        func(*args, **kwargs)
+
+        try:
+            os.remove('test.db')
+        except OSError:
+            pass
+
+    return _wrap
+
+def set_pragmas(cur):
+    cur.execute("PRAGMA foreign_keys = ON;")
+
+class SQLTester(object):
+    """This class actually creates an sql database
+    and tries to create all the tables and triggers
+    you have defined. That way you can verify that
+    your schema actually works (in theory) without
+    building your android project."""
+
+    def __init__(self):
+        self.tables = []
+        self.triggers = []
+
+    def add_tables(self, *sqltables):
+        self.tables.extend(sqltables)
+
+    def add_triggers(self, *triggers):
+        self.triggers.extend(triggers)
+
+    @clear_db
+    def test_create(self):
+        """Try creating all tables and triggers"""
+        con = sql.connect('test.db')
+        con.row_factory = sql.Row
+        with con:
+            cur = con.cursor()
+            set_pragmas(cur)
+
+            for table in self.tables:
+                print("\n", table)
+                cur.execute(str(table))
+
+            for trigger in self.triggers:
+                print("\n", trigger)
+                cur.execute(str(trigger))
---
+++ sample_test.py
@@ -0,0 +1,40 @@
+
+from AndroidCodeGenerator.generator import Generator
+from AndroidCodeGenerator.db_table import (Table, Column, ForeignKey, Unique,
+                                           Trigger)
+from AndroidCodeGenerator.database_triggers import DatabaseTriggers
+from AndroidCodeGenerator.sql_validator import SQLTester
+
+persons = Table('Person').cols(Column('firstname').text.not_null.default("''"),\
+                               Column('lastname').text.not_null.default("''"),\
+                               Column('bio').text.not_null.default("''"))\
+                         .constraints(Unique('firstname').on_conflict_replace)
+
+log = Table('Log').cols(Column('pId').integer.not_null,
+                        Column('firstname').text.not_null,
+                        Column('lastname').text.not_null,
+                        Column('bio').text.not_null,
+                        Column('time').timestamp.default_current_timestamp)
+
+# Create a trigger that keeps the log up to date
+# I recommend using temp triggers unless you see a performance hit
+trigger = Trigger('tr_log').temp.if_not_exists
+
+# Here I have given the wrong table name on purpose. Notice that the sql
+# creation will fail because there is no table named bob.
+trigger.after.update_on("bob")
+#trigger.after.update_on(log.name)
+
+# Raw sql
+trigger.do_sql("INSERT INTO {table} ({cols}) VALUES\
+ ({oldcols})".format(table=log.name,
+                     cols=log.list_column_names(exclude=['_id','time']),
+                     oldcols=persons.list_column_names(exclude=[],
+                                                       prefix="old.")))
+
+# Let's try to create the SQL
+st = SQLTester()
+st.add_tables(persons, log)
+st.add_triggers(trigger)
+
+st.test_create()
